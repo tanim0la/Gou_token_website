52ceba78d03bcba32d5207333aa53fea192c5c31
tanim0la <privatelife147@gmail.com>
2022-07-02 15:55:06 +0100

    new update

--- components/Airdrop.js
+++ components/Airdrop.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import airdrop from '../public/airdrop.png'
 import Web3 from 'web3'
@@ -7,18 +7,16 @@ import Modal from './Modal'
 
 let web3
 
-class Airdrop extends Component {
-  state = {
-    address: '',
-    addy: '',
-    errMessage: '',
-    buttonMsg: 'Join Whitelist',
-    eligible: false,
-    modalOn: false,
-    bool: false,
-  }
+function Airdrop() {
+  const [address, setAddress] = useState('')
+  const [addy, setAddy] = useState('')
+  const [errMessage, setErrMessage] = useState('')
+  const [buttonMsg, setButtonMsg] = useState('Join Whitelist')
+  const [eligible, setEligible] = useState(false)
+  const [modalOn, setModalOn] = useState(false)
+  const [bool, setBool] = useState(false)
 
-  onClaim = async () => {
+  const onClaim = async () => {
     web3 = new Web3(window.ethereum)
     const accounts = await web3.eth.getAccounts()
     if (accounts.length == 0) {
@@ -27,107 +25,105 @@ class Airdrop extends Component {
       )
       web3 = new Web3(provider)
     }
-    this.setState({ errMessage: '', bool: true })
+    setErrMessage('')
+    setBool(true)
     try {
       const check = await gou.methods
         .whitelistedAddresses(this.state.address)
         .call()
 
       if (check) {
-        this.setState({ addy: this.state.address, modalOn: true })
+        setAddy(address)
+        setModalOn(true)
       } else {
-        this.setState({
-          eligible: true,
-          errMessage:
-            'You are not Eligible for the airdrop, join whitelist above',
-        })
+        setEligible(true)
+        setErrMessage(
+          'You are not Eligible for the airdrop, join whitelist above',
+        )
       }
     } catch (err) {
-      this.setState({ errMessage: err.message })
+      setErrMessage(err.message)
     }
-    this.setState({ address: '', bool: false })
+    setAddress('')
+    setBool(false)
   }
 
-  onJoin = async () => {
+  const onJoin = async () => {
     web3 = new Web3(window.ethereum)
     const accounts = await web3.eth.getAccounts()
 
-    this.setState({ buttonMsg: 'Please wait...', errMessage: '', bool: true })
+    setButtonMsg('Please wait...')
+    setErrMessage('')
+    setBool(true)
     try {
       await gou.methods.whitelistAddress().send({
         from: accounts[0],
       })
     } catch (err) {
-      this.setState({ errMessage: err.message })
+      setErrMessage(err.message)
     }
 
-    this.setState({
-      buttonMsg: 'Join Whitelist',
-      address: '',
-      eligible: false,
-      bool: false,
-    })
+    setButtonMsg('Join Whitelist')
+    setAddress('')
+    setEligible(false)
+    setBool(false)
   }
 
-  offModal = (mode) => {
+  const offModal = (mode) => {
     this.setState({ modalOn: mode })
   }
 
-  render() {
-    return (
-      <div className="flex flex-col justify-center items-center sm:flex-row">
-        <div className="">
-          <div className="h-80 w-80 sm:h-96 sm:w-96">
-            <Image draggable={false} src={airdrop} />
-          </div>
+  return (
+    <div className="flex flex-col justify-center items-center sm:flex-row">
+      <div className="">
+        <div className="h-80 w-80 sm:h-96 sm:w-96">
+          <Image draggable={false} src={airdrop} />
         </div>
-        <div className="flex flex-col justify-center w-full items-center sm:items-start sm:pt-32">
-          <span className="text-xl font-semibold pb-4 italic sm:text-2xl sm:pb-10">
-            Are you eligible?
-          </span>
-          <input
-            hidden={this.state.eligible}
-            className="border-0 border-b-2 border-stone-400 w-4/5 bg-transparent rounded-br-sm rounded-bl-sm pb-1"
-            placeholder=" Enter Address 0x..."
-            value={this.state.address}
-            onChange={(e) => this.setState({ address: e.target.value })}
-          />
-
-          <div className="py-4">
-            {!this.state.eligible ? (
-              <button
-                className="rounded-lg border-2 border-red-800 hover:border-green-400 p-2 text-sm transition delay-100 duration-300 ease-in-out font-semibold"
-                onClick={this.onClaim}
-                disabled={this.state.bool}
-              >
-                Check Eligibility
-              </button>
-            ) : (
-              <button
-                className="rounded-lg border-2 border-red-800 hover:border-green-400 p-2 text-sm transition delay-100 duration-300 ease-in-out font-semibold"
-                onClick={this.onJoin}
-                disabled={this.state.bool}
-              >
-                {this.state.buttonMsg}
-              </button>
-            )}
-          </div>
-
-          {this.state.errMessage ? (
-            <span className="border-0 border-red-800 text-red-400 px-4 sm:px-0">
-              <span className="font-semibold text-2xl">Oops...</span>
-              <br />
-              {this.state.errMessage}
-            </span>
-          ) : null}
-        </div>
-
-        {this.state.modalOn && (
-          <Modal offModal={this.offModal} address={this.state.addy} />
-        )}
       </div>
-    )
-  }
+      <div className="flex flex-col justify-center w-full items-center sm:items-start sm:pt-32">
+        <span className="text-xl font-semibold pb-4 italic sm:text-2xl sm:pb-10">
+          Are you eligible?
+        </span>
+        <input
+          hidden={eligible}
+          className="border-0 border-b-2 border-stone-400 w-4/5 bg-transparent rounded-br-sm rounded-bl-sm pb-1"
+          placeholder=" Enter Address 0x..."
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+
+        <div className="py-4">
+          {!this.state.eligible ? (
+            <button
+              className="rounded-lg border-2 border-red-800 hover:border-green-400 p-2 text-sm transition delay-100 duration-300 ease-in-out font-semibold"
+              onClick={onClaim}
+              disabled={bool}
+            >
+              Check Eligibility
+            </button>
+          ) : (
+            <button
+              className="rounded-lg border-2 border-red-800 hover:border-green-400 p-2 text-sm transition delay-100 duration-300 ease-in-out font-semibold"
+              onClick={onJoin}
+              disabled={bool}
+            >
+              {buttonMsg}
+            </button>
+          )}
+        </div>
+
+        {this.state.errMessage ? (
+          <span className="border-0 border-red-800 text-red-400 px-4 sm:px-0">
+            <span className="font-semibold text-2xl">Oops...</span>
+            <br />
+            {errMessage}
+          </span>
+        ) : null}
+      </div>
+
+      {modalOn && <Modal offModal={offModal} address={addy} />}
+    </div>
+  )
 }
 
 export default Airdrop
